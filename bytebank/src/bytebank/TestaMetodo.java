e146fcfef9e875f9939d7de3054f866728393b76
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-02 12:19:58 -0300

    Métodos com retorno

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -6,7 +6,12 @@ public class TestaMetodo {
 		
 		Conta contaDoPaulo = new Conta();
 		contaDoPaulo.saldo = 100;
+		
 		contaDoPaulo.deposita(50);
 		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 }
