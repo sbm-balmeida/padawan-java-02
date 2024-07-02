16195e4d382308114529b58704f670b6c22c4ff3
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-02 15:45:10 -0300

    Static

--- bytebankEncapsulado/src/bytebankEncapsulado/TestaValores.java
+++ bytebankEncapsulado/src/bytebankEncapsulado/TestaValores.java
@@ -11,6 +11,12 @@ public class TestaValores {
 //		conta.setNumero(-330);
 		
 		System.out.println(conta.getAgencia());
+		
+		Conta conta2 = new Conta(1337, 16549);
+		Conta conta3 = new Conta(2112, 14660);
+		
+		System.out.println(Conta.getTotal());
+
 	}
 
 }
